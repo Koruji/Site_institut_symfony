f674a80f7a7c661faf7316d85dff8b877d07f5f1
Koruji <jadecottin212@gmail.com>
2025-01-17 14:34:45 +0100

    Ajout des boutons gestion admin Ajout prof par admin

--- src/Controller/ProfesseurController.php
+++ src/Controller/ProfesseurController.php
@@ -3,14 +3,18 @@
 namespace App\Controller;
 
 use App\Entity\Professeur;
+use App\Entity\User;
 use App\Form\ProfesseurType;
 use App\Repository\ProfesseurRepository;
+use App\Service\GenerateMatricule;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
 use Symfony\Component\Routing\Attribute\Route;
 use Symfony\Component\Security\Core\Authentication\Token\Storage\TokenStorageInterface;
+use Symfony\Component\Security\Http\Attribute\IsGranted;
 
 final class ProfesseurController extends AbstractController
 {
@@ -23,17 +27,36 @@ final class ProfesseurController extends AbstractController
     }
 
     #[Route('/professeur/ajout_professeur', name: 'app_professeur_new', methods: ['GET', 'POST'])]
-    public function new(Request $request, EntityManagerInterface $entityManager): Response
+    #[IsGranted("ROLE_ADMIN")]
+    public function new(Request $request,
+                        EntityManagerInterface $entityManager,
+                        UserPasswordHasherInterface $userPasswordHasher): Response
     {
         $professeur = new Professeur();
-        $form = $this->createForm(ProfesseurType::class, $professeur);
+        $user = new User();
+        $matricule = (new GenerateMatricule($entityManager))->generateMatricule();
+
+        $form = $this->createForm(ProfesseurType::class, $professeur, [
+            'hide_matricule' => true,
+            'matricule' => $matricule,
+        ]);
         $form->handleRequest($request);
 
         if ($form->isSubmitted() && $form->isValid()) {
+            $user->setNom($professeur->getNom());
+            $user->setPrenom($professeur->getPrenom());
+            $user->setEmail($professeur->getEmail());
+            $user->setRoles(['ROLE_PROFESSEUR']);
+            $user->setIdProfesseur($professeur);
+
+            $mdp = $form->get('mdp')->getData();
+            $user->setPassword($userPasswordHasher->hashPassword($user, $mdp));
+            $entityManager->persist($user);
+
             $entityManager->persist($professeur);
             $entityManager->flush();
 
-            return $this->redirectToRoute('app_professeur_index', [], Response::HTTP_SEE_OTHER);
+            return $this->redirectToRoute('app_admin_dashboard', [], Response::HTTP_SEE_OTHER);
         }
 
         return $this->render('professeur/new.html.twig', [
@@ -53,14 +76,19 @@ final class ProfesseurController extends AbstractController
     #[Route('/professeur/{id}/modification_compte', name: 'app_professeur_edit', methods: ['GET', 'POST'])]
     public function edit(Request $request, Professeur $professeur, EntityManagerInterface $entityManager): Response
     {
-        $form = $this->createForm(ProfesseurType::class, $professeur);
+        $user = $this->getUser();
+
+        $form = $this->createForm(ProfesseurType::class, $professeur,  [
+            'hide_mdp' => true, // Cache le champ mdp
+            'mdp_actuel' => $user->getPassword(),
+        ]);
         $form->handleRequest($request);
 
         if ($form->isSubmitted() && $form->isValid()) {
-            $user = $this->getUser();
             $user->setEmail($professeur->getEmail());
             $user->setPrenom($professeur->getPrenom());
             $user->setNom($professeur->getNom());
+            //TODO: ajouter la possibilité de modifier son mot de passe
 
             $entityManager->persist($user);
             $entityManager->flush();
--- src/Form/ProfesseurType.php
+++ src/Form/ProfesseurType.php
@@ -6,6 +6,8 @@ use App\Entity\Matiere;
 use App\Entity\Professeur;
 use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\HiddenType;
+use Symfony\Component\Form\Extension\Core\Type\PasswordType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
@@ -13,8 +15,15 @@ class ProfesseurType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
+            if ($options['hide_matricule']) {
+                $builder->add('matricule', HiddenType::class, [
+                    'data' => $options['matricule'],
+                ]);
+            } else {
+                $builder->add('matricule');
+            };
+
         $builder
-            ->add('matricule')
             ->add('nom')
             ->add('prenom')
             ->add('email')
@@ -24,12 +33,30 @@ class ProfesseurType extends AbstractType
                 'required' => false,
             ])
         ;
+
+        if ($options['hide_mdp']) {
+            $builder->add('mdp', HiddenType::class, [
+                'data' => $options['mdp_actuel'],
+                'mapped' => false,
+            ]);
+        } else {
+            $builder->add('mdp', PasswordType::class, [
+                'label' => 'Mot de passe',
+                'mapped' => false,
+                'required' => true,
+                //TODO : implémenter le fait que le mdp soit obligatoire
+            ]);
+        }
     }
 
     public function configureOptions(OptionsResolver $resolver): void
     {
         $resolver->setDefaults([
             'data_class' => Professeur::class,
+            'hide_mdp' => false,
+            'hide_matricule' => false,
+            'matricule' => null,
+            'mdp_actuel' => null,
         ]);
     }
 }
